59d5c29b37f79aacc9e3f0422bdbc743ec2eedc2
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-07-21 14:16:56 +0530

    commmit02

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 
+import express from "express"
+import dotenv from "dontenv"
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 6000
+
+app.get("/", (req, res)=>{
+    return res.json({message:"server started"})
+})
+
+app.listen(PORT , ()=>{
+    console.log(`server runnning on port ${PORT}`)
+})

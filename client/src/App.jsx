144afa8005b161333562e62534d21c13f65b136b
Jyoti Chaubey <jyotichaubey2005@gmail.com>
2026-07-24 02:09:04 +0530

    commit12

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import { linkWithCredential } from 'firebase/auth'
+import { log } from 'firebase/firestore/lite/pipelines'
 
 export const ServerUrl = "http://localhost:8000"
 
 const App = () => {
+  useEffect(()=>{
+    const getUser = async () => {
+      try {
+        const result = await axios.get(ServerUrl + "/api/user/current-user",
+          {linkWithCredential:true})
+
+        console.log(result.data)
+        
+      } catch (error) {
+        console.log(error)
+        
+      }
+    }
+    getUser()
+  },[])
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
